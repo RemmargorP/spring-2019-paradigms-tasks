1bc98f8115be10e01453e3b04a6fce9e478fc5d0
Vadim Salavatov <xremmargorpx@gmail.com>
2019-03-29 22:37:11 +0300

    Add model.py, test_model.py from task04

--- task06/model.py
+++ task06/model.py
@@ -4,7 +4,18 @@ import abc
 
 class Scope:
     def __init__(self, parent=None):
-        raise NotImplementedError
+        self.variables = {}
+        self.parent = parent
+
+    def __getitem__(self, key):
+        if key in self.variables:
+            return self.variables[key]
+        if self.parent is not None:
+            return self.parent[key]
+        raise KeyError(key)
+
+    def __setitem__(self, key, value):
+        self.variables[key] = value
 
 
 class ASTNode(metaclass=abc.ABCMeta):
@@ -29,11 +40,29 @@ class Number(ASTNode):
     быть можно положить в словарь в качестве ключа (см. специальные методы
     __eq__, __ne__, __hash__ — требуется реализовать две из них).
     """
+
     def __init__(self, value):
-        pass
+        assert isinstance(value, int)
+        self.value = value
 
     def evaluate(self, scope):
-        raise NotImplementedError
+        return self
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.value == other
+        if isinstance(other, Number):
+            return self.value == other.value
+        raise TypeError
+
+    def __hash__(self):
+        return self.value
+
+    def __bool__(self):
+        return bool(self.value)
+
+    def __str__(self):
+        return str(self.value)
 
 
 class Function(ASTNode):
@@ -47,11 +76,13 @@ class Function(ASTNode):
 
     Аналогично Number, метод evaluate должен возвращать self.
     """
+
     def __init__(self, args, body):
-        pass
+        self.args = args
+        self.body = body
 
     def evaluate(self, scope):
-        raise NotImplementedError
+        return self
 
 
 class FunctionDefinition(ASTNode):
@@ -63,76 +94,14 @@ class FunctionDefinition(ASTNode):
     обновление текущего Scope,  т.е. в него добавляется новое значение типа
     Function под заданным именем, а возвращать evaluate должен саму функцию.
     """
+
     def __init__(self, name, function):
-        pass
+        self.name = name
+        self.function = function
 
     def evaluate(self, scope):
-        raise NotImplementedError
-
-
-class Conditional(ASTNode):
-    """
-    Представляет ветвление в программе, т. е. if.
-
-    condition - это некоторое выражение, результат вычисления которого
-        обязательно является объектом типа Number.
-    if_true и if_false - списки (возможно, пустые или равные None) выражений.
-
-    Если результат вычисления condition - это объект Number, содержащий 0, то
-    вычисляется if_false список, иначе if_true.
-
-    Результатом вычисления всего Conditional является результат вычисления
-    последнего элемента в соответствующем (if_true или if_false) списке.
-
-    Если соответствующий список пуст или равен None, то возвращаемое значение
-    остается на ваше усмотрение.
-    """
-    def __init__(self, condition, if_true, if_false=None):
-        pass
-
-    def evaluate(self, scope):
-        raise NotImplementedError
-
-
-class Print(ASTNode):
-    """
-    Печатает значение выражения на отдельной строке.
-
-    В методе evaluate вычисляется значение выражения expr, после чего
-    на экран выводится число, хранящееся внутри результата вычисления (это
-    гарантированно Number).
-
-    Вывод завершается переходом на следующую строку (никаких дополнительных
-    символов, лишнего форматирования, научных форматов, отступов и пр).
-
-    Возвращаемое значение метода evаluate - объект типа Number, который был
-    выведен.
-    """
-    def __init__(self, expr):
-        pass
-
-    def evaluate(self, scope):
-        raise NotImplementedError
-
-
-class Read(ASTNode):
-    """
-    Читает число из стандартного потока ввода и обновляет текущий Scope.
-
-    Метод evaluate читает со стандартного потока ввода число (на отдельной
-    строке) и добавляет в scope это число под именем name.
-
-    evaluate должен возвращать объект типа Number, представляющий прочитанное
-    число.
-
-    Каждое входное число располагается на отдельной строке (никаких пустых
-    строк и лишних символов не будет).
-    """
-    def __init__(self, name):
-        pass
-
-    def evaluate(self, scope):
-        raise NotImplementedError
+        scope[self.name] = self.function
+        return self.function
 
 
 class FunctionCall(ASTNode):
@@ -159,11 +128,106 @@ class FunctionCall(ASTNode):
     метода evaluate. Если результат вычисления последнего выражения
     неопределён, то возвращаемое значение остаётся на ваше усмотрение.
     """
+
     def __init__(self, fun_expr, args):
-        pass
+        self.fun_expr = fun_expr
+        self.args = args
 
     def evaluate(self, scope):
-        raise NotImplementedError
+        function = self.fun_expr.evaluate(scope)
+        call_scope = Scope(scope)
+
+        for (arg_name, arg) in zip(function.args, self.args):
+            call_scope[arg_name] = arg.evaluate(scope)
+
+        last_result = Number(0)
+
+        for op in function.body:
+            last_result = op.evaluate(call_scope)
+
+        return last_result
+
+
+class Conditional(ASTNode):
+    """
+    Представляет ветвление в программе, т. е. if.
+
+    condition - это некоторое выражение, результат вычисления которого
+        обязательно является объектом типа Number.
+    if_true и if_false - списки (возможно, пустые или равные None) выражений.
+
+    Если результат вычисления condition - это объект Number, содержащий 0, то
+    вычисляется if_false список, иначе if_true.
+
+    Результатом вычисления всего Conditional является результат вычисления
+    последнего элемента в соответствующем (if_true или if_false) списке.
+
+    Если соответствующий список пуст или равен None, то возвращаемое значение
+    остается на ваше усмотрение.
+    """
+
+    def __init__(self, condition, if_true, if_false=None):
+        self.condition = condition
+        self.if_true = if_true
+        self.if_false = if_false
+
+    def evaluate(self, scope):
+        branch = self.if_true if bool(self.condition.evaluate(
+            scope)) else self.if_false
+
+        last_result = Number(0)
+
+        for op in branch or []:
+            last_result = op.evaluate(scope)
+
+        return last_result
+
+
+class Print(ASTNode):
+    """
+    Печатает значение выражения на отдельной строке.
+
+    В методе evaluate вычисляется значение выражения expr, после чего
+    на экран выводится число, хранящееся внутри результата вычисления (это
+    гарантированно Number).
+
+    Вывод завершается переходом на следующую строку (никаких дополнительных
+    символов, лишнего форматирования, научных форматов, отступов и пр).
+
+    Возвращаемое значение метода evаluate - объект типа Number, который был
+    выведен.
+    """
+
+    def __init__(self, expr):
+        self.expr = expr
+
+    def evaluate(self, scope):
+        result = self.expr.evaluate(scope)
+        print(result)
+        return result
+
+
+class Read(ASTNode):
+    """
+    Читает число из стандартного потока ввода и обновляет текущий Scope.
+
+    Метод evaluate читает со стандартного потока ввода число (на отдельной
+    строке) и добавляет в scope это число под именем name.
+
+    evaluate должен возвращать объект типа Number, представляющий прочитанное
+    число.
+
+    Каждое входное число располагается на отдельной строке (никаких пустых
+    строк и лишних символов не будет).
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def evaluate(self, scope):
+        value = Number(int(input()))
+        scope[self.name] = value
+        return value
 
 
 class Reference(ASTNode):
@@ -172,11 +236,12 @@ class Reference(ASTNode):
     Метод evaluate должен найти в scope объект с именем name и вернуть его
     (см. подробнее про класс Scope).
     """
+
     def __init__(self, name):
-        pass
+        self.name = name
 
     def evaluate(self, scope):
-        raise NotImplementedError
+        return scope[self.name]
 
 
 class BinaryOperation(ASTNode):
@@ -199,11 +264,41 @@ class BinaryOperation(ASTNode):
     Гарантируется, что lhs и rhs при вычислении дадут объект типа Number,
     т.е. не может получиться так, что вам придется сравнивать две функции.
     """
+
+    __OPERATORS = {
+        '+': lambda x, y: Number(x.value + y.value),
+        '-': lambda x, y: Number(x.value - y.value),
+        '*': lambda x, y: Number(x.value * y.value),
+        '/': lambda x, y: Number(x.value // y.value),
+        '%': lambda x, y: Number(x.value % y.value),
+        '==': lambda x, y: Number(int(x.value == y.value)),
+        '!=': lambda x, y: Number(int(x.value != y.value)),
+        '<': lambda x, y: Number(int(x.value < y.value)),
+        '>': lambda x, y: Number(int(x.value > y.value)),
+        '<=': lambda x, y: Number(int(x.value <= y.value)),
+        '>=': lambda x, y: Number(int(x.value >= y.value)),
+        '&&': lambda x, y: Number(int(bool(x) and bool(y))),
+        '||': lambda x, y: Number(int(bool(x) or bool(y))),
+    }
+
     def __init__(self, lhs, op, rhs):
-        pass
+        self.lhs = lhs
+        self.op = op
+        self.rhs = rhs
+
+        if op not in BinaryOperation.__OPERATORS:
+            raise SyntaxError(f'operator {op} is not supported')
 
     def evaluate(self, scope):
-        raise NotImplementedError
+        left = self.lhs.evaluate(scope)
+        right = self.rhs.evaluate(scope)
+
+        if not isinstance(left, Number):
+            raise TypeError('Left operand is not a Number')
+        if not isinstance(right, Number):
+            raise TypeError('Right operand is not a Number')
+
+        return BinaryOperation.__OPERATORS[self.op](left, right)
 
 
 class UnaryOperation(ASTNode):
@@ -217,8 +312,17 @@ class UnaryOperation(ASTNode):
     Как и для BinaryOperation, Number, хранящий 0, считаем за False, а все
     остальные за True.
     """
+
     def __init__(self, op, expr):
-        pass
+        self.op = op
+        self.expr = expr
 
     def evaluate(self, scope):
-        raise NotImplementedError
+        result = self.expr.evaluate(scope)
+
+        if self.op == '-':
+            return Number(-result.value)
+        if self.op == '!':
+            return Number(int(not bool(result)))
+
+        return SyntaxError(f'unary operator {op} is not supported')
--- task06/test_model.py
+++ task06/test_model.py
@@ -1,32 +1,202 @@
 #!/usr/bin/env python3
 import pytest
+
 from model import *
 
 
-def test_construction():
-    FunctionDefinition('fac', Function(['n'], [
-        Conditional(
-            BinaryOperation(Reference('n'), '=', Number(0)),
-            [Number(1)],
-            [
-                BinaryOperation(
-                    Reference('n'),
-                    '*',
-                    FunctionCall(Reference('fac'), [
-                        BinaryOperation(
-                            Reference('n'),
-                            '-',
-                            Number(1)
-                        )
-                    ])
-                )
-            ]
-        )
-    ]))
-    Read('n')
-    Print(
-        UnaryOperation('-', FunctionCall(Reference('fac'), [Reference('n')]))
-    )
+def test_scope_ok():
+    scope = Scope()
+
+    b = [1, 2, 3]
+
+    scope['a'] = 3
+    scope['b'] = b
+
+    assert scope['a'] is 3
+    assert scope['b'] is b
+
+
+def test_scope_exception():
+    scope = Scope()
+
+    scope['a'] = 3
+    scope['123'] = 'a'
+
+    with pytest.raises(KeyError):
+        scope['hello']
+
+
+def test_scope_parent():
+    d = {123: 456, 'top': 'bottom'}
+
+    parent = Scope()
+    parent['a'] = d
+    parent['b'] = 4
+
+    scope = Scope(parent)
+    scope['a'] = 5
+
+    assert scope['a'] is 5
+    assert scope['b'] is 4
+    assert parent['a'] is d
+
+    with pytest.raises(KeyError):
+        scope['hello']
+
+
+def test_scope_grand_parent():
+    grand = Scope()
+    parent = Scope(grand)
+    scope = Scope(parent)
+
+    var_a = '123'
+    var_b = 32
+
+    grand['a'] = var_a
+    grand['b'] = var_b
+
+    parent['a'] = 239
+
+    scope['c'] = 123
+
+    assert scope['a'] is 239
+    assert scope['b'] is var_b
+    assert scope['c'] is 123
+
+
+def test_number():
+    with pytest.raises(TypeError):
+        n = Number()
+
+    a = Number(3)
+    b = Number(4)
+
+    assert a != b
+    assert a == Number(3)
+    assert bool(Number(1)) is True
+    assert bool(Number(0)) is False
+    assert a.evaluate(Scope()) is a
+
+
+def test_function():
+    f = Function(['a', 'b'], [])
+    assert f.evaluate(Scope()) is f
+
+
+def test_function_definition():
+    scope = Scope()
+    func = Function(['a', 'b'], [])
+    FunctionDefinition('func', func).evaluate(scope)
+
+    assert scope['func'] is func
+
+
+def test_function_call():
+    scope = Scope()
+    assert FunctionCall(Function([], [Number(42)]), []
+                        ).evaluate(scope) == Number(42)
+
+
+def test_conditional():
+    assert Conditional(Number(0), [Number(239)], [
+                       Number(30)]).evaluate(Scope()) == 30
+    assert Conditional(Number(1), [Number(239)], [
+                       Number(30)]).evaluate(Scope()) == 239
+    assert Conditional(Number(1), None, []).evaluate(Scope()) == 0
+    assert Conditional(Number(0), None, []).evaluate(Scope()) == 0
+
+
+def test_print(capsys):
+    Print(Number(239**2)).evaluate(Scope())
+
+    assert int(capsys.readouterr().out) == 239**2
+
+
+def test_read(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda: '42')
+    scope = Scope()
+    Read('a').evaluate(scope)
+    assert scope['a'] == 42
+
+
+def test_reference():
+    scope = Scope()
+
+    a = Number(37)
+    scope['a'] = a
+
+    assert Reference('a').evaluate(scope) is a
+
+
+def test_binary_operator():
+    arr = [0, 30, 45, 239, 1337, 228, 1997]
+    brr = [2, 52, 34, 1, 9, 88, 728]
+    s = Scope()
+
+    for a in arr:
+        for b in brr:
+            assert BinaryOperation(
+                Number(a), '+', Number(b)).evaluate(s) == a + b
+            assert BinaryOperation(
+                Number(a), '-', Number(b)).evaluate(s) == a - b
+            assert BinaryOperation(
+                Number(a), '*', Number(b)).evaluate(s) == a * b
+            assert BinaryOperation(
+                Number(a), '/', Number(b)).evaluate(s) == a // b
+            assert BinaryOperation(
+                Number(a), '%', Number(b)).evaluate(s) == a % b
+            assert BinaryOperation(
+                Number(a), '==', Number(b)).evaluate(s) == (a == b)
+            assert BinaryOperation(
+                Number(a), '!=', Number(b)).evaluate(s) == (a != b)
+            assert BinaryOperation(
+                Number(a), '<', Number(b)).evaluate(s) == (a < b)
+            assert BinaryOperation(
+                Number(a), '>', Number(b)).evaluate(s) == (a > b)
+            assert BinaryOperation(
+                Number(a), '<=', Number(b)).evaluate(s) == (a <= b)
+            assert BinaryOperation(
+                Number(a), '>=', Number(b)).evaluate(s) == (a >= b)
+            assert BinaryOperation(Number(a), '&&', Number(
+                b)).evaluate(s) == (bool(a) and bool(b))
+            assert BinaryOperation(Number(a), '||', Number(
+                b)).evaluate(s) == (bool(a) or bool(b))
+
+
+def test_unary_operator():
+    assert UnaryOperation('-', Number(3)).evaluate(Scope()) == -3
+    assert UnaryOperation('-', Number(-2000)).evaluate(Scope()) == 2000
+    assert bool(UnaryOperation('!', Number(0)).evaluate(Scope())) is True
+    assert bool(UnaryOperation('!', Number(23)).evaluate(Scope())) is False
+
+
+def test_factorial():
+    scope = Scope()
+
+    FunctionDefinition('factorial', Function(['n'], [
+        Conditional(BinaryOperation(Reference('n'), '!=', Number(1)),
+                    [BinaryOperation(Reference('n'), '*',
+                                     FunctionCall(Reference('factorial'),
+                                                  [BinaryOperation(
+                                                      Reference('n'),
+                                                      '-',
+                                                      Number(1))
+                                                   ]))],
+                    [Number(1)])
+    ])).evaluate(scope)
+
+    assert FunctionCall(Reference('factorial'), [
+                        Number(1)]).evaluate(scope) == 1
+    assert FunctionCall(Reference('factorial'), [
+                        Number(2)]).evaluate(scope) == 2
+    assert FunctionCall(Reference('factorial'), [
+                        Number(3)]).evaluate(scope) == 6
+    assert FunctionCall(Reference('factorial'), [
+                        Number(4)]).evaluate(scope) == 24
+    assert FunctionCall(Reference('factorial'), [
+                        Number(5)]).evaluate(scope) == 120
+    assert FunctionCall(Reference('factorial'), [
+                        Number(10)]).evaluate(scope) == 3628800
 
 
 if __name__ == "__main__":
