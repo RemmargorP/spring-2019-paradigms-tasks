43f0d5c41482bbe1548dc6d9a672cb62e0aaf49a
Vadim Salavatov <xremmargorpx@gmail.com>
2019-05-03 15:19:40 +0300

    task10

--- task10/test/TestBasics.hs
+++ task10/test/TestBasics.hs
@@ -11,14 +11,23 @@ testsBasics = testGroup "Unit tests for Basics tasks"
     [testCase "head' works on non-empty list" $
         head' [1,2,3] @?= 1
 
+    , testCase "head' works on infinite list" $
+        head' [1..] @?= 1
+
     , testCase "tail' works on non-empty list too" $
         tail' [1,2,3] @?= [2,3]
 
     , testCase "take' takes 1 element from 3-element list" $
         take' 1 [1,2,3] @?= [1]
 
+    , testCase "take' takes 5 elements from infinite list" $
+        take' 5 [3..] @?= [3, 4, 5, 6, 7]
+
     , testCase "drop' drops 1 element from 3-element list" $
         drop' 1 [1,2,3] @?= [2,3]
+
+    , testCase "drop' drops 3 elements from infinite list" $
+        head' (drop' 3 [1..]) @?= 4
 
     , testCase "filter' selects only even numbers from 0 to 10" $
         filter' even [0..10] @?= [0,2..10]
@@ -28,6 +37,9 @@ testsBasics = testGroup "Unit tests for Basics tasks"
 
     , testCase "concat' works on finite lists as expected" $
         concat' [1,2,3] [4,5,6] @?= [1..6]
+
+    , testCase "concat' works on infinite lists" $
+        take' 10 (concat' [1,2,3,4] [5..]) @?= [1..10]
 
     , testCase "quickSort actualy sorts the list" $
         quickSort' [5,2,3,4,1] @?= [1..5]
--- task10/test/TestRobots.hs
+++ task10/test/TestRobots.hs
@@ -9,9 +9,47 @@ main = defaultMain testsRobots
 testsRobots :: TestTree
 testsRobots = let
         walter = robot "Walter" 50 50
+        mywalter = robot "Walter" 50 123
+        dummy = robot "Dummy" 1 0
+        grandmaster = robot "Grand Master Kolbaster" 99 1337
     in testGroup "Unit tests for Robots task"
         [ testCase "Test for getName" $
             getName walter @?= "Walter"
+
+        , testCase "Test for getAttack" $
+            getAttack walter @?= 50
+
+        , testCase "Test for getHealth" $
+            getHealth mywalter @?= 123
+
+        , testCase "Test for setName" $
+            setName "Roflan" mywalter @?= robot "Roflan" 50 123
+
+        , testCase "Test for setAttack" $
+            setAttack 30 mywalter @?= robot "Walter" 30 123
+
+        , testCase "Test for setHealth" $
+            setHealth 239 mywalter @?= robot "Walter" 50 239
+
         , testCase "Test for printRobot" $
             printRobot walter @?= "Walter, attack: 50, health: 50"
+
+        , testCase "Test for damage" $
+            damage mywalter 100 @?= robot "Walter" 50 23
+
+        , testCase "Test for isAlive" $ do
+            isAlive mywalter @?= True
+            isAlive dummy @?= False
+
+        , testCase "Test for fight" $ do
+            fight mywalter dummy @?= robot "Dummy" 1 (-50)
+            fight dummy mywalter @?= mywalter
+
+        , testCase "Test for threeRoundFight" $ do
+            threeRoundFight mywalter dummy @?= mywalter
+            threeRoundFight mywalter grandmaster @?= grandmaster
+            threeRoundFight grandmaster (robot "Ex-Grand Master" (337+99+99) 1000) @?= grandmaster
+
+        , testCase "Test for survivors" $ -- Для остальных функций из шага 4 не имеет смысла делать тесты.
+            survivors @?= [robot "Alpha" 123 456, robot "Gamma" 301 1337]
         ]
