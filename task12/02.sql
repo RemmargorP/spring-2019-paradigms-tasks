28b82671001e86008462eb2f103c147133af7a20
Vadim Salavatov <xremmargorpx@gmail.com>
2019-05-15 23:37:50 +0300

    style fix

--- task12/02.sql
+++ task12/02.sql
@@ -1,4 +1,10 @@
 -- Выведите название страны с максимальным уровнем грамотности по последним
 -- данным, которые доступны для страны. В выводе: название страны и уровень
 -- грамотности, именно в таком порядке и без лишних полей. (0,75 баллов)
-SELECT Name FROM LiteracyRate JOIN Country ON CountryCode=Code GROUP BY Code HAVING MAX(Year) ORDER BY Rate DESC LIMIT 1;
+SELECT Name 
+  FROM LiteracyRate 
+  JOIN Country ON CountryCode=Code 
+  GROUP BY Code 
+  HAVING MAX(Year) 
+  ORDER BY Rate DESC 
+  LIMIT 1;
--- task12/05.sql
+++ task12/05.sql
@@ -1,3 +1,7 @@
 -- Выведите форму правления с максимальной суммарной площадью стран, которые её
 -- придерживаются (вывод: форма правления и суммарная площадь). (0,25 баллов)
-SELECT GovernmentForm, SUM(SurfaceArea) FROM Country GROUP BY GovernmentForm ORDER BY SUM(SurfaceArea) DESC LIMIT 1;
+SELECT GovernmentForm, SUM(SurfaceArea) 
+  FROM Country 
+  GROUP BY GovernmentForm 
+  ORDER BY SUM(SurfaceArea) DESC 
+  LIMIT 1;
