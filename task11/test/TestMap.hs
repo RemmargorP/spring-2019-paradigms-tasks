f0f8140d5d6c991d92d62b7a49bcf9c4c2836120
Vadim Salavatov <xremmargorpx@gmail.com>
2019-05-18 21:30:38 +0300

    Map implemented

--- task11/test/TestMap.hs
+++ task11/test/TestMap.hs
@@ -220,8 +220,8 @@ testNaiveTree = testGroup "Test NaiveTree" [
 testMap :: TestTree
 testMap = testGroup "Testing implementations of trees"
     [
-        mapTests "Data.Map.Strict" (Proxy :: Proxy SMap.Map)
-        -- mapTests "NaiveList" (Proxy :: Proxy NaiveList)
+        mapTests "Data.Map.Strict" (Proxy :: Proxy SMap.Map),
+        mapTests "NaiveList" (Proxy :: Proxy NaiveList)
         -- mapTests "NaiveTree" (Proxy :: Proxy NaiveTree)
         -- testNaiveTree
     ]
